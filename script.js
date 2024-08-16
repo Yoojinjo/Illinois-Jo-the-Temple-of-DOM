663c2533eb3416363aa8747c33d96fd5fc678102
Yoojinjo <eugenejo111@gmail.com>
2024-08-16 12:05:45 -0500

    added functionality to direction button to modify the text are

---
+++ script.js
@@ -0,0 +1,12 @@
+/* made some buttons. clicking on directions will modify the text area to explain the game. clicking on the start button will hide the buttons and begin the game loop. 
+features needed. a score tracker, a life counter that updates.
+game loop?
+game over condition */
+
+const directionButton = document.getElementById("directions")
+const startButton = document.getElementById("start")
+const textArea = document.getElementById("text")
+
+
+
+directionButton.addEventListener("click", ()=>{textArea.innerHTML ="testcode"})
